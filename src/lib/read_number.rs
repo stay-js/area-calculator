575e6720798e1a75bcd8482d77abec0b501317bd
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-27 15:16:04 +0100

    simplify read-number

--- src/lib/read_number.rs
+++ src/lib/read_number.rs
@@ -1,28 +1,22 @@
 use dialoguer::{theme::ColorfulTheme, Input};
 
 pub fn read_number(prompt: &str) -> f64 {
-    let mut number = -1.0;
-
-    while number < 0.0 {
-        number = Input::with_theme(&ColorfulTheme::default())
-            .with_prompt(prompt)
-            .validate_with(|input: &String| {
-                return match input.parse::<f64>() {
-                    Ok(num) => {
-                        if num < 0.0 {
-                            return Err("Must be bigger than 0");
-                        }
-
-                        return Ok(());
+    return Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .validate_with(|input: &String| {
+            return match input.parse::<f64>() {
+                Ok(num) => {
+                    if num < 0.0 {
+                        return Err("Must be bigger than 0");
                     }
-                    Err(_) => Err("Failed to parse input"),
-                };
-            })
-            .interact_text()
-            .expect("Failed to read input")
-            .parse()
-            .expect("Failed to parse input");
-    }
 
-    return number;
+                    return Ok(());
+                }
+                Err(_) => Err("Failed to parse input"),
+            };
+        })
+        .interact_text()
+        .expect("Failed to read input")
+        .parse()
+        .expect("Failed to parse input");
 }
